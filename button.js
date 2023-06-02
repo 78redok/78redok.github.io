f314d64b627764c97e945f28a366f33537af18f0
78redok <81646320+78redok@users.noreply.github.com>
2023-06-01 17:07:02 -0700

    made some edits to css and js

--- button.js
+++ button.js
@@ -1,6 +1,6 @@
 function download(){
-    if (confirm ('Resume access disabled.')) {
-        // location.href = './resume.pdf';
+    if (confirm ('Thank you for your consideration!')) {
+        location.href = './resume.pdf';
         return false;
     }
     return false;
